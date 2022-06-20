495c6718a06ebdacdcfb2d50c4f75be84dbf017a
Susan Winters <susanwinters214@gmail.com>
2022-06-20 09:25:29 -0500

    created Javascript variables for Coding Bootcamp

---
+++ app.js
@@ -0,0 +1,19 @@
+console.log("Hello World!\n==========\n");
+console.log(
+  "Follow the steps in the README.md file to complete the exercises:\n==========\n"
+);
+
+// Exercise 1
+const firstName = "Susan";
+let lastName = "Winters";
+var age = 21;
+
+// Exercise 2
+let fullName = `${firstName} ${lastName}`;
+console.log(fullName);
+
+// Exercise 3
+let city = "Dallas";
+let passtime = "playing with my dog"
+let myStory = `Hello! My name is ${fullName}. I live in ${city}. I enjoy ${passtime} and coding!`;
+console.log(myStory);
